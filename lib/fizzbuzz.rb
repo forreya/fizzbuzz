ba3bf04bc27db1dd58ffbc200066a31fd69ac42f
terryhycheng <terryhycheng@gmail.com>
2023-01-12 12:06:03 +0000

    Added multiple_three & multiple_five. Added extra tests.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,20 @@
 
 def fizzbuzz(number)
-  'fizz'
+  # Check if the number is a multiple of both 3 and 5
+  # Check if the number is a multiple of 3
+  if multiple_three(number)
+    'fizz'
+  # Check if the number is a multiple of 5
+  elsif multiple_five(number)
+    'buzz'
+  # returns the number if nothing matches
+  end
+end
+
+def multiple_three(number)
+  number % 3 == 0
+end
+
+def multiple_five(number)
+  number % 5 == 0
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,5 +7,17 @@ describe 'fizzbuzz' do
     # when 3 is passed into fizzbuzz, return 'fizz'
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns the number when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
 end
 
